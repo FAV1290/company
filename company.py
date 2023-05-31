6045b1745e9bfe95e22bf82755f72c24ed9ec1c1
Alexey <fav1290@gmail.com>
2023-05-31 10:37:51 +0300

    Added couple of quality of life features

--- company.py
+++ company.py
@@ -1,6 +1,14 @@
 import statistics
 
 
+def merge_all_departments_employees(departments_list):
+    employees_list = []
+    for department in departments_list:
+        for employee in department['employers']:
+            employees_list.append(employee)
+    return employees_list
+
+
 # Вывести названия всех отделов
 def print_departments_titles(departments_list):
     print('The company has following departments:')
@@ -24,29 +32,29 @@ def print_all_employees(departments_list, with_department=False):
 # Вывести имена всех сотрудников компании, которые получают больше 100к
 def print_adjusted_sum_and_more_gainers(departments_list, adjusted_salary):
     print(f"Following employees' salary outnumber {adjusted_salary} rubles:")
-    for department in departments_list:
-        for employee in department['employers']:
-            if employee['salary_rub'] >= adjusted_salary:
-                print(f"• {employee['first_name']} {employee['last_name']}")
+    employees_list = merge_all_departments_employees(departments_list)
+    for employee in employees_list:
+        if employee['salary_rub'] >= adjusted_salary:
+            print(f"• {employee['first_name']} {employee['last_name']}")
 
 
 # Вывести позиции, на которых люди получают меньше 80к (можно с повторениями)
 # Вывести названия должностей, которые получают больше 90к без повторений
 def print_positions_with_specific_salary(departments_list, adjusted_salary, min_or_max):
+    employees_list = merge_all_departments_employees(departments_list)
     handled_positions = []
     if min_or_max == min:
         adjective = 'below'
     else:
         adjective = 'above'        
     print(f'Following postions are offering salary {adjective} {adjusted_salary} rubles:')
-    for department in departments_list:
-        for employee in department['employers']:
-            salary = employee['salary_rub']
-            position = employee['position']
-            if salary == min_or_max(salary, adjusted_salary) and salary != adjusted_salary:
-                if position not in handled_positions: 
-                    handled_positions.append(position)
-                    print(f"• {position}")
+    for employee in employees_list:
+        salary = employee['salary_rub']
+        position = employee['position']
+        if salary == min_or_max(salary, adjusted_salary) and salary != adjusted_salary:
+            if position not in handled_positions: 
+                handled_positions.append(position)
+                print(f"• {position}")
 
 
 # Посчитать, сколько денег в месяц уходит на каждый отдел – и вывести вместе с названием отдела
@@ -70,17 +78,17 @@ def print_minimal_salary_in_departments(departments_list):
 def salary_indicators_in_departments(departments_list):
     salary_indicators = []
     for department in departments_list:
-        minimal = min([employee['salary_rub'] for employee in department['employers']])
-        average = statistics.mean([employee['salary_rub'] for employee in department['employers']])
-        median = statistics.median([employee['salary_rub'] for employee in department['employers']])
-        maximal = max([employee['salary_rub'] for employee in department['employers']])
+        minimal_salary = min([employee['salary_rub'] for employee in department['employers']])
+        average_salary = statistics.mean([employee['salary_rub'] for employee in department['employers']])
+        median_salary = statistics.median([employee['salary_rub'] for employee in department['employers']])
+        maximal_salary = max([employee['salary_rub'] for employee in department['employers']])
         salary_indicators.append(
             {
                 'department' : department['title'],
-                'minimal' : minimal,
-                'average' : average,
-                'median' : int(median),
-                'maximal': maximal,
+                'minimal salary' : minimal_salary,
+                'average salary' : average_salary,
+                'median salary' : int(median_salary),
+                'maximal salary': maximal_salary,
             }
         )
     return salary_indicators
@@ -90,50 +98,53 @@ def print_salary_indicators(departments_list):
     salary_indicators = salary_indicators_in_departments(departments_list)
     for department_indicators in salary_indicators:
         print(f"Salary indicators for {department_indicators['department']}:")
-        print(f"• Minimal salary: {department_indicators['minimal']} rubles")
-        print(f"• Average salary: {department_indicators['average']} rubles")
-        print(f"• Median salary: {department_indicators['median']} rubles")
-        print(f"• Maximal salary: {department_indicators['maximal']} rubles")
+        print(f"• Minimal salary: {department_indicators['minimal salary']} rubles")
+        print(f"• Average salary: {department_indicators['average salary']} rubles")
+        print(f"• Median salary: {department_indicators['median salary']} rubles")
+        print(f"• Maximal salary: {department_indicators['maximal salary']} rubles")
         print()
 
 
 # Вывести среднюю зарплату по всей компании
 def average_salary(departments_list):
-    requirements, headcount = 0, 0
-    for department in departments_list:
-        requirements += sum([employee['salary_rub'] for employee in department['employers']])
-        headcount += len(department['employers'])
-    return requirements // headcount
+    employees_list = merge_all_departments_employees(departments_list)
+    salaries_sum = sum([employee['salary_rub'] for employee in employees_list])
+    headcount = len(employees_list)
+    return salaries_sum // headcount
 
 
 # Посчитать среднюю зарплату по каждому отделу среди девушек (их зовут Мишель, Николь, Кристина и Кейтлин)
 def print_average_salary_among_women(departments_list, female_names):
     print('Women average salary in every department of the company:')
     for department in departments_list:
-        requirements, headcount = 0, 0
+        salaries_sum, headcount = 0, 0
         for employee in department['employers']:
             if employee['first_name'] in female_names:
-                requirements += employee['salary_rub']
+                salaries_sum += employee['salary_rub']
                 headcount += 1
         if headcount == 0:
             print(f"• There's no women in {department['title']}")
         else:
-            average_salary = requirements // headcount
+            average_salary = salaries_sum // headcount
             print(f"• {department['title']}: {average_salary} rubles")
     
 
-# Вывести без повторений имена людей, чьи фамилии заканчиваются на гласную букву. 
-def vowel_ending_last_names(departments_list):
+# Вывести без повторений имена людей, чьи фамилии заканчиваются на гласную букву
+def vowel_ending_last_names_bearers(departments_list):
+    employees_list = merge_all_departments_employees(departments_list)
     vowels = ('a', 'e', 'i', 'o', 'u', 'w', 'y')
-    used_names = []
+    vowel_enders = set()
+    for employee in employees_list:
+        if employee['last_name'][-1] in vowels:
+            vowel_enders.add(employee['first_name'])
+    return vowel_enders
+
+
+def print_vowel_ending_last_name_bearers_names(departments_list):
+    vowel_enders = sorted(vowel_ending_last_names_bearers(departments_list))
     print('There are names of employees, whose last names end with a vovel:')
-    for department in departments_list:
-        for employee in department['employers']:
-            last_name = employee['last_name']
-            first_name = employee['first_name']
-            if last_name[-1] in vowels and first_name not in used_names:
-                used_names.append(first_name)
-                print(f"• {employee['first_name']}")
+    for name in vowel_enders:
+        print(f'• {name}')
 
 
 def main():
@@ -184,7 +195,8 @@ def main():
     print()
     print_average_salary_among_women(departments, female_names)
     print()
-    vowel_ending_last_names(departments)
+    print_vowel_ending_last_name_bearers_names(departments)
+
 
 if __name__ == '__main__':
     main()
